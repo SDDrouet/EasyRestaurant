760b369e8b70b45633918f0ef4caa3d2eb22c0e0
sddrouet <sddrouet@espe.edu.ec>
2026-01-01 21:23:51 -0500

    update frontend initial project

--- frontend/src/App.tsx
+++
@@ -1,7 +0,0 @@
-export default function App() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-white px-4 py-24">
-      <h1 className="text-4xl font-bold text-gray-900">Hello, World!</h1>
-    </main>
-  );
-}
